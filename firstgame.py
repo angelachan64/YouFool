3d538f141127c59593de5b9719cc22b6d7b340b5
Tonierstraw1865 <henryadin1@me.com>
2015-10-24 11:16:08 -0400

    screen

--- firstgame.py
+++ firstgame.py
@@ -1,6 +1,39 @@
-import sys.pygame
+import pygame,sys
 
 pygame.init()
 
-size= width, height = 1336, 768
+size= width, height = 320, 240
+
+black = 0,0,0
+
+screen = pygame.display.set_mode(1336, 768)
+
+title =pygame.image.load("title.png").convert()
+
+new =pygame.image.load.load("newbutton.png").convert()
+
+resume =pygame.image.load("resumebutton.png").convert()
+
+instructions =pygame.image.load("instructions.png")
+    titlerect=titlerect.move(160,120)
+
+    newrect=newrect.move(200,150)
+
+    resumerect=resumerect.move(250,150)
+
+    instructionsrect=instructionsrect.move(100,50)
+
+    screen.fill(black)
+
+    screen.blit(title, titlerect)
+
+    screen.blit(new, newrect)
+
+    screen.blit(resume, resumerect)
+
+    screen.blit(instructions, instructionsrect)
+
+    pygame.display.flip()
+
+    
 
