d568135b495ccb7c95032d92a955e6eff71cbf34
Tonierstraw1865 <henryadin1@me.com>
2015-11-14 10:38:35 -0500

    added text

--- updatedfirstgame.py
+++ updatedfirstgame.py
@@ -6,7 +6,7 @@ size= width, height = 800, 600
 black = 0,0,0
 screen = pygame.display.set_mode(size)
 title =pygame.image.load("titletrans.bmp").convert()
-new =pygame.image.load("menu_button.bmp").convert()
+'''new =pygame.image.load("menu_button.bmp").convert()'''
 resume =pygame.image.load("resumebutton.bmp").convert()
 titlerect = title.get_rect()
 newrect = new.get_rect()
@@ -19,7 +19,8 @@ screen.blit(title, titlerect)
 screen.blit(new, newrect)
 screen.blit(resume, resumerect)
 pygame.display.flip()
-
+'''
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+'''
