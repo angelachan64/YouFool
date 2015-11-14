b4aee684f76a644566a05537d7dc95264492eca4
Jason Shin <jasonshin98@gmail.com>
2015-11-14 11:05:07 -0500

    updated code

--- updatedfirstgame.py
+++ updatedfirstgame.py
@@ -1,26 +1,30 @@
 import pygame,sys
-
 pygame.init()
 
 size= width, height = 800, 600
 black = 0,0,0
 screen = pygame.display.set_mode(size)
-title =pygame.image.load("titletrans.bmp").convert()
-'''new =pygame.image.load("menu_button.bmp").convert()'''
-resume =pygame.image.load("resumebutton.bmp").convert()
-titlerect = title.get_rect()
-newrect = new.get_rect()
-resumerect = resume.get_rect()
-titlerect=titlerect.move(width/2 - 207.5,120)
-newrect=newrect.move(width/2-196.5,300)
-resumerect=resumerect.move(width/2 - 122.5,400)
+loaded = []
+rects = []
+loaded.append(pygame.image.load("static/youfool.png").convert())
+loaded.append(pygame.image.load("static/menu_button.bmp").convert())
+loaded.append(pygame.image.load("static/resume_button.bmp").convert())
+for i in range(0,3):
+    rects.append(loaded[i].get_rect())
+rects[0]=rects[0].move(width/2 - 207.5,120)
+rects[1]=rects[1].move(width/2 - 196.5, 300)
+rects[2]=rects[2].move(width/2 - 196.5,400)
 screen.fill(black)
-screen.blit(title, titlerect)
-screen.blit(new, newrect)
-screen.blit(resume, resumerect)
+for i in range(0,3):
+    screen.blit(loaded[i], rects[i])
 pygame.display.flip()
-'''
+titlescreen = True
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-'''
+    if titlescreen:
+        if (rects[1].collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]):
+            screen.fill(black)
+            pygame.display.flip()
+            titlescreen = False
