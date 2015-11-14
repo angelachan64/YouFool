843bf741d86cf3916af59240bb32656f20c192ab
Jason Shin <jasonshin98@gmail.com>
2015-11-14 11:19:45 -0500

    updated code

--- updatedfirstgame.py
+++ updatedfirstgame.py
@@ -6,21 +6,27 @@ black = 0,0,0
 screen = pygame.display.set_mode(size)
 loaded = []
 rects = []
-loaded.append(pygame.image.load("static/youfool.png").convert())
-loaded.append(pygame.image.load("static/menu_button.bmp").convert())
-loaded.append(pygame.image.load("static/resume_button.bmp").convert())
-for i in range(0,3):
-    rects.append(loaded[i].get_rect())
-rects[0]=rects[0].move(width/2 - 207.5,120)
-rects[1]=rects[1].move(width/2 - 196.5, 300)
-rects[2]=rects[2].move(width/2 - 196.5,400)
-screen.fill(black)
-for i in range(0,3):
-    screen.blit(loaded[i], rects[i])
-pygame.display.flip()
 titlescreen = True
+titlescreenloaded = True
+
+def introscreen():
+    loaded.append(pygame.image.load("static/youfool.png").convert())
+    loaded.append(pygame.image.load("static/menu_button.bmp").convert())
+    loaded.append(pygame.image.load("static/resume_button.bmp").convert())
+    for i in range(0,3):
+        rects.append(loaded[i].get_rect())
+    rects[0]=rects[0].move(width/2 - 207.5,120)
+    rects[1]=rects[1].move(width/2 - 196.5, 300)
+    rects[2]=rects[2].move(width/2 - 196.5,400)
+    screen.fill(black)
+    for i in range(0,3):
+        screen.blit(loaded[i], rects[i])
+    pygame.display.flip()
 
 while 1:
+    if titlescreenloaded:
+        introscreen()
+        titlescreenloaded = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     if titlescreen:
