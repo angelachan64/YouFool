3fc2917f3d4e9e7de3f22532df8e3b911cc0528d
Jason Shin <jasonshin98@gmail.com>
2015-12-05 10:14:34 -0500

    tried implementing sprites

---
+++ sprites/sprites.py
@@ -0,0 +1,26 @@
+import pygame,sys
+pygame.init()
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, image):
+       pygame.sprite.Sprite.__init__(self)
+       self.image = pygame.image.load(image).convert_alpha()
+       #self.size = size
+       self.rect = self.image.get_rect()
+       
+    #def update(self, ):
+        
+
+size = width, height = 800, 600
+black = 0,0,0
+screen = pygame.display.set_mode(size)
+basicsprite = Block("titletrans2.bmp")
+#basicsprite.__init__(self, "../static/titletrans2.bmp")
+basicgroup = pygame.sprite.Group()
+basicgroup.add(basicsprite)
+print basicgroup.sprites()
+basicgroup.draw(screen)
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
