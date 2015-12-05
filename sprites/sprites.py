6db797e624301e63c742505ca6354229dabf9048
Jason Shin <jasonshin98@gmail.com>
2015-12-05 10:47:55 -0500

    first image loaded

--- sprites/sprites.py
+++ sprites/sprites.py
@@ -1,13 +1,13 @@
 import pygame,sys
 pygame.init()
 
-class Block(pygame.sprite.Sprite):
-    def __init__(self, image):
+class basicsprite(pygame.sprite.Sprite):
+    def __init__(self, image, coords):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image).convert_alpha()
         #self.size = size
         self.rect = self.image.get_rect()
-        self.rect = self.rect.move(width/2 - 207.5,80)
+        self.rect = self.rect.move(coords)
 
        
     #def update(self, ):
@@ -16,10 +16,10 @@ class Block(pygame.sprite.Sprite):
 size = width, height = 800, 600
 black = 0,0,0
 screen = pygame.display.set_mode(size)
-basicsprite = Block("titletrans2.bmp")
+title = basicsprite("../static/titletrans2.bmp", (width/2 - 207.5,80))
 #basicsprite.__init__(self, "../static/titletrans2.bmp")
 basicgroup = pygame.sprite.Group()
-basicgroup.add(basicsprite)
+basicgroup.add(title)
 basicgroup.draw(screen)
 pygame.display.flip()
 
